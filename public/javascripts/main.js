14e7e3b3f38251be63b9da0555218da8ee9cddc3
Jen Fong-Adwent <jen@ednapiranha.com>
2012-05-03 11:43:35 -0700

    added stub responses for listing tracks

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,16 +12,21 @@ $(function() {
     });
   });
 
+  $('#tracks.list').on('click', 'li', function() {
+    document.location.href = $(this).data('src');
+  });
+
   var updateTrack = function(data) {
-    var track = $('<li><div class="track-info"><img src=""><span class="artist-name">' +
-      '</span><span class="track-title"></span></div><div class="user-info"><img src="">' +
-      '<span class="posted"></span></div><audio controls="controls" preload="none" autobuffer>' +
-      '<source src="" type="audio/ogg" /></audio></li>');
+    var track = $('<li data-src="/track/detail/' + data.id + '"><div class="track-info">' +
+      '<div class="image-wrapper"><img src=""></div><div class="details"><span class="artist-name">' +
+      '</span><span class="track-title"></span></div></div><div class="user-info"><img src="">' +
+      '<span class="posted"></span><audio controls="controls" preload="none" autobuffer>' +
+      '<source src="" type="audio/ogg" /></audio></div></li>');
     track.find('.track-info img').attr('src', data.artistImage);
-    track.find('.track-info .artist-name').text(data.artist);
-    track.find('.track-info .track-title').text(data.trackTitle);
+    track.find('.track-info .artist-name').text('Artist: ' + data.artist);
+    track.find('.track-info .track-title').text('Title: ' + data.trackTitle);
     track.find('.user-info img').attr('src', data.user);
-    track.find('.user-info .posted').text(data.created);
+    track.find('.user-info .posted').text('Posted on: ' + data.created);
     track.find('source').attr('src', data.trackSource);
 
     trackItems.prepend(track);
@@ -29,5 +34,13 @@ $(function() {
 
   $.get('/track/1', function(data) {
     updateTrack(data);
+  });
+
+  socket.on('connect', function () {
+    socket.on('message', function (data) {
+      for (var i = 0; i < data.length; i ++) {
+        updateTrack(data[i].track);
+      }
+    });
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,50 @@
 var gravatar = require('gravatar');
 
-module.exports = function(app) {
-  /* Filter for checking if a user is authenticated before
-   * they can access views that require login
-   */
-  var isAuthenticated = function(req, res, next) {
-    if (!req.session.email) {
-      res.redirect('/');
-    } else {
-      next();
-    }
-  };
-
+module.exports = function(app, io) {
   // Home/main
   app.get('/', function(req, res) {
     res.render('index', { title: 'Herbie' });
   });
 
   // Dashboard
-  app.get('/dashboard', isAuthenticated, function(req, res) {
-    res.render('dashboard', { title: 'Dashboard' });
+  app.get('/dashboard', function(req, res) {
+    res.render('dashboard', { title: 'Herbie: Dashboard' });
+  });
+
+  // Add track
+  app.get('/tracks/list', function(req, res) {
+    // stub
+    var tracks = [{
+      track: {
+        'id': req.params.id,
+        'artist': 'Autechre',
+        'artistImage': '/samples/sample.jpg',
+        'trackTitle': 'Clipper',
+        'trackSource': '/samples/sample.ogg',
+        'user': gravatar.url(req.session.email),
+        'created': new Date()
+      }
+    }];
+
+    io.sockets.emit('message', tracks);
+
+    res.json({ status: 200 });
+  });
+
+  // Get track details
+  app.get('/track/detail/:id', function(req, res) {
+    // stub
+    var track = {
+      'id': req.params.id,
+      'artist': 'Autechre',
+      'artistImage': '/samples/sample.jpg',
+      'trackTitle': 'Clipper',
+      'trackSource': '/samples/sample.ogg',
+      'user': gravatar.url(req.session.email),
+      'created': new Date()
+    };
+
+    res.render('details', { title: track.artist + ': ' + track.trackTitle, track: track });
   });
 
   // Get track
