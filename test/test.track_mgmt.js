f3a76fab5a79ba63bbf5f433fda8a3afdb946564
Jen Fong-Adwent <jen@ednapiranha.com>
2012-05-09 20:55:22 -0400

    added track listing test

---
+++ test/test.track_mgmt.js
@@ -0,0 +1,32 @@
+var assert = require('should');
+var nock = require('nock');
+var trackMgmt = require('../lib/track_mgmt');
+
+describe('trackMgmt', function() {
+  describe('.getAll', function() {
+    it('returns a list of tracks from the user', function() {
+      var req = {
+        session: {
+          email: 'test@test.org'
+        }
+      };
+
+      var body = {
+        songs: [{
+          artist: 'Aphex Twin',
+          small_art_url: 'http://test.com/223.jpg',
+          album_art_url: 'http://test.com/223.jpg',
+          album: 'Selected Ambient Works Vol. 2',
+          track: 'Blue Calx',
+          s3_ogg_url: 'http://test.com/223.ogg'
+        }]
+      };
+
+      trackMgmt.getAll(req, JSON.stringify(body), function(err, tracks) {
+        tracks[0].track.artist.should.equal(body.songs[0].artist);
+        tracks[0].track.album.should.equal(body.songs[0].album);
+        tracks.length.should.equal(1);
+      });
+    });
+  });
+});
